1831b22751141ae1e68f4106ceb0978978d9ed8d
RobbinZhu <robbin.zhu@uuabc.com>
2018-12-20 10:40:52 +0800

    remove unused code

--- game/main_scene.js
+++
@@ -1,2 +0,0 @@
-import Scene from '../common/scene.js'
-export default Scene.create('main')
--- game/resolution.js
+++
@@ -1,4 +0,0 @@
-import Resolution from '../common/resolution.js'
-const resolution = Resolution.create()
-resolution.setResolution(1024, 768, 0.75, 2.5)
-export default resolution
